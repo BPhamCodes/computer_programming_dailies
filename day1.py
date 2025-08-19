ecd55e40fcafc9a7c37b72c4653b1fd685662db5
Bpham <bp.bells29@gmail.com>
2025-08-19 14:28:32 -0700

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,12 @@
+# write a function that traverses a list and a random name from that list
+# bonus if you write in nested loops for traversal, concetanation of results, etc.
+import random
+
+list1 = ['A', 'B', 'C']
+
+def gen_rand_numb(list):
+    random_choice_1 = random.choice(list)
+    print(random_choice_1)
+
+gen_rand_numb(numbers)
+#print (gen_rand_numb(numbers))
